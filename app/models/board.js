0d02badd3a6e32de3cb952bd2b8ca08115b696fb
Tom <teryun93@gmail.com>
2016-03-26 01:23:42 -0700

    changes to board api

--- app/models/board.js
+++ app/models/board.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BoardSchema   = new Schema({
-    title: {type: String, index: {unique: true}},
+    title: String,
     description: String,
     creator: {
         type: mongoose.Schema.Types.ObjectId,
