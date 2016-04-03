62ac0a02df4e28d32045f3f4cb1032ec4062cb0d
Tom <teryun93@gmail.com>
2016-04-03 13:26:38 -0700

    test1

--- server.js
+++ server.js
@@ -344,7 +344,10 @@ router.route('/boards/:board_id/workflows')
         
         Workflow.find({ 'board' :  (req.params.board_id) }).populate('creator').populate('tasks').populate({
                 path: 'tasks',
-                populate: { path: 'assignee', 'creator' }
+                populate: { path: 'assignee'}
+            }).populate({
+                path: 'tasks',
+                populate: { path: 'creator'}
             }).exec(function(error, workflows) {
             res.json(workflows);});
         });
